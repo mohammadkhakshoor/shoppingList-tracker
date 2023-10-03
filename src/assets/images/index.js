9e33e804e1e3125e03b36a88072b462a6ecad2b9
mohammad khakshoor <mkhbeats@gmail.com>
2023-10-03 15:56:54 +0330

    CREATE: add icons for dark mode

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,18 +1,24 @@
 import github from "./github.svg";
-import backgroundSVG from "./bgc.svg";
+// import backgroundSVG from "./bgc.svg";
 
 import searchIcon from "./search-icon.svg";
 import filterIcon from "./filter-icon.svg";
 import sortIcon from "./sort-icon.svg";
 import addIcon from "./add.svg";
 import removeIcon from "./remove.svg";
+import searchIconDark from "./search-icon-dark.svg";
+import filterIconDark from "./filter-icon-dark.svg";
+import sortIconDark from "./sort-icon-dark.svg";
 
 export {
   github,
-  backgroundSVG,
+  // backgroundSVG,
   searchIcon,
   sortIcon,
   filterIcon,
   addIcon,
   removeIcon,
+  sortIconDark,
+  filterIconDark,
+  searchIconDark,
 };
